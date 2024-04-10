58276535452daea7c5892dc3df03a183d9cb794e
Cassidy Hardisty <ch843@byu.edu>
2024-04-10 16:07:45 -0600

    added keys to the db models

--- INTEX2.0/Models/ProductsCategory.cs
+++ INTEX2.0/Models/ProductsCategory.cs
@@ -1,11 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace INTEX2._0.Models;
 
 public partial class ProductsCategory
 {
+    [ForeignKey("Category")]
     public string CategoryId { get; set; } = null!;
 
+    public virtual Category Category { get; set; }
+
+    
+    [ForeignKey("Products")]
     public int ProductId { get; set; }
+    public virtual Products Products { get; set; }
 }
