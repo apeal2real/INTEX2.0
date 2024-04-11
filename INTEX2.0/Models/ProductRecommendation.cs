7400837aff6e58a8ce4cd65d7add85432644af99
apeal2real <alec.peal@gmail.com>
2024-04-11 13:25:53 -0600

    455 tables

---
+++ INTEX2.0/Models/ProductRecommendation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace INTEX2._0.Models;
+
+public partial class ProductRecommendation
+{
+    public int ItemId { get; set; }
+
+    public int? RecommendedId1 { get; set; }
+
+    public int? RecommendedId2 { get; set; }
+
+    public int? RecommendedId3 { get; set; }
+}
