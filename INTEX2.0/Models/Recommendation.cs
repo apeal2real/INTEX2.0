00c27c75ac8539d2b825c87d9db5311d07b36386
ElainaHalls <130600410+ElainaHalls@users.noreply.github.com>
2024-04-11 18:31:08 -0600

    more changes

---
+++ INTEX2.0/Models/Recommendation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace INTEX2._0.Models;
+
+public partial class Recommendation
+{
+    public int CustomerId { get; set; }
+
+    public int? Recommendation1 { get; set; }
+
+    public int? Recommendation2 { get; set; }
+
+    public int? Recommendation3 { get; set; }
+
+    public int? Recommendation4 { get; set; }
+}
